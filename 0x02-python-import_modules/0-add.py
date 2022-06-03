8a378fed5d303e1b64a7e116aec5711563e21c6b
Chris7-code <mbonimpachristian@gmail.com>
2022-06-03 01:14:21 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    """
-
-    Prints the result of the addition between two numbes
-
-    """
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a,b, add(a,b)))
